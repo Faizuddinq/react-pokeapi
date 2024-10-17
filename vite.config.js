350007972c814eb44b6ddfbee4d0aa3152327339
Faizuddinq <faizqureshi6126@gmail.com>
2024-10-17 11:46:34 +0530

    completed detail page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
+git branch -M mainimport { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
+//done
